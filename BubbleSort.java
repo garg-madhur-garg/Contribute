33e5b5e441a8c803489353a0a1b824f675fef3fe
ANKIT KUMAR <79157735+Ankit-kumar19@users.noreply.github.com>
2021-10-01 20:39:57 +0530

    Update BubbleSort.java

--- BubbleSort.java
+++ BubbleSort.java
@@ -1,27 +1,30 @@
+
 package dataStructure;
 
 public class BubbleSort {
 	public static void main(String[] args) {
-		
+		//Initializing an Array 
+		//You can Also take input from user using Scanner
 		int[] intArray = {10 , 20 ,11, -4, 6, -22 ,33};
-		
+		//Traversing the array using for loop
 		for (int unsortedArrayIndex = intArray.length-1 ; unsortedArrayIndex>0 ; unsortedArrayIndex--) {
 			
 			for (int i=0; i<unsortedArrayIndex;i++) {
-				
+				// swaping element which is greater with the smaller one 
 				if (intArray[i]>intArray[i+1]) {
 					swap(intArray , i , i+1);
 				}
 			}
 			
 		}
+		//Printing The Sorted Array
 		for(int j=0;j<intArray.length;j++) {
 			System.out.println(intArray[j]);
 		}
 		
 		
 	}
-	
+	//Swapping function for swapping the elements
 	public static void swap(int[] arr , int i , int j) {
 		
 		if(i==j) {
